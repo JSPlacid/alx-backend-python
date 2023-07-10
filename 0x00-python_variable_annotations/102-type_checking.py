1567ea3cac158d9cc7359303ea70b7ba0855471a
unknown <jspacific777@gmail.com>
2023-07-10 16:57:01 +0100

    python type annotation task

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,33 +1,22 @@
 #!/usr/bin/env python3
-"""
-python3 -c
-'print(__import__("102-type_checking").__doc__)'
-Use mypy to validate the following piece of code and apply any necessary changes.
-
-Author: Oluwaseun Ayela 
+"""Use mypy to validate the following piece of code
+and apply any necessary changes.
 """
 
-from typing import List, Tuple
+from typing import Tuple, List, Any
 
 
-def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> Tuple[int, ...]:
-    """
-    Trying this docstring for this module
-     python3 -c
-     'print(__import__("102-type_checking").zoom_array.__doc__)'
-
-    """
-    zoomed_in: List[int] = [
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """Corrected annotations"""
+    zoomed_in: List = [
         item for item in lst
-        for _ in range(factor)
+        for i in range(factor)
     ]
-    return tuple(zoomed_in)
+    return zoomed_in
 
 
-array: Tuple[int, ...] = (12, 72, 91)
+array = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
 
 zoom_3x = zoom_array(array, 3)
-
-print(__import__("102-type_checking").zoom_array.__doc__)
